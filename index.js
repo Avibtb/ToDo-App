aaff2f2b9ebd9645fe24d8309e8aded123cb1ac9
Avibtb <avi.singh.iit01@gmail.com>
2021-05-15 16:09:44 +0545

    Added the main file, imported the express and initialize the server

---
+++ index.js
@@ -0,0 +1,15 @@
+//import express
+const express = require('express');
+const app = express();
+
+//allocating port for the server
+const port = 8000;
+
+//Starting the server
+app.listen(port, function(err){
+    if(err){
+        console.log(`Error in runnig the server${err}`);
+        return;
+    }
+    console.log(`Server is successfully running at port : ${port}`);
+});
